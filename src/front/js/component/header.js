ad8e4120c6f1ebd59c660a4b4840ff18d8df725c
robfowler4 <rfowler97@gmail.com>
2022-04-14 21:34:36 +0000

    landing-page

--- src/front/js/component/header.js
+++ src/front/js/component/header.js
@@ -7,13 +7,22 @@ import "../../styles/style.css"
 export const Header = () => {
     return (
         <header className="d-flex flex-column">
-            <div className="carousel-wrapper" id="cf">
-                <div className="d-flex justify-content-start ms-5 mt-5"><Link to="/"> <i className="fab fa-hackerrank red1 life-hack">Life<span className="black1">Hack</span></i></Link></div>
-                <h3 className="header-quote d-flex justify-content-start ms-5">'Success is the progressive realization of predetermined, worthwhile, personal goals'</h3>
-                <div className="login-button d-flex justify-content-start ms-5 ">
-                    <Link to="/patientlogin">
-                        <button className="pt-login-btn btn btn-primary d-flex justify-content-center">Login</button>
-                    </Link>
+            <div className="header-img bg-dark" id="cf">
+                <div className="header-background">
+                    <div className="container">
+                        <div className="d-flex justify-content-start ms-5 mt-5">
+                            <h1 className="white1">Lets us help you <span className="red1 font2">HACK</span> into your <span className="">HEALTH</span></h1>
+                        </div>
+                        <div>
+                            <h3 className="header-quote d-flex justify-content-start ms-5">'Success is the progressive realization of predetermined, worthwhile, personal goals'</h3>
+                        </div>
+                        <div className="">
+                            <Link className="linnk landing-btn" to="/patientlogin">
+                                <button className="ms-5 pt-login-btn btn exer-button white1">Begin Here</button>
+                            </Link>
+                        </div>
+                    </div>
+
                 </div>
             </div>
 
--- src/front/js/pages/landingpage.js
+++ src/front/js/pages/landingpage.js
@@ -20,8 +20,38 @@ export const LandingPage = () => {
         <div className="app-wrapper">
             <Navbar />
             <div className="intro-logo jumbo-bg">
-                <Header />
+                <div><Header /></div>
+                <div className="landing-middle-div d-flex justify-content-center">
+                    <div className="landing-object">
+                        <div className="d-flex justify-content-center"><img className="landing-middle-img" src="https://www.pinclipart.com/picdir/big/159-1597497_calendar-calendar-icon-png-round-clipart.png"></img></div>
+                        <div className="d-flex justify-content-center"><h1>Calendar</h1></div>
+                        <div className="d-flex justify-content-center"><p className="landing-middle-text d-flex justify-content-center">Hack into your Time by using our calendar, to schedule your appointments and your weekly workouts.</p></div>
+                        <div className="">
+                            <Link className="linnk landing-btn d-flex justify-content-center" to="/patientlogin">
+                                <button className=" pt-login-btn btn exer-button white1">Begin Here</button>
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+                <div className="landing-body d-flex justify-content-center mb-5">
+                    <div className="landing-body-background d-flex justify-content-center">
+                        <div>
+                            <div><h1 className="d-flex justify-content-center landing-h1">Workout Tutorials</h1></div>
+                            <div className="d-flex justify-content-center landing-text"><p>Over 1,000 certified exercises with demonstrations for whether you're at home or on the road.</p></div>
+                            <div className="d-flex justify-content-center"><img className="workout-img d-flex justify-content-center" src="https://www.keppnerboxing.com/wp-content/themes/KeppnerBoxing/images/man-doing-push-ups-with-medicine-ball.png"></img></div>
+                        </div>
+                    </div>
+                    <div className="landing-body-background2 d-flex justify-content-center">
+                        <div className="fruit-img">
+                            <div className="img-overlay">
+                                <h1 className="white1 d-flex justify-content-center pt-4">Nutritional Information</h1>
+                                <p className="white1 d-flex justify-content-center landing-text2">Healthy facts to help improve diet as well as performance during every day workout.</p>
+                            </div>
+                        </div>
 
+                    </div>
+
+                </div>
             </div>
         </div>
     )
